2975bfd03f3e8cbe8e43ab04b3cf6ed386f38003
Samurai <dan931901@gmail.com>
2024-11-06 17:30:10 +0300

    CRUD done

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 """
-1)Нужно добавить проверку на имя, чтобы вызывать конструтор того или иного класса в CRUD
-2)Добавить аналог static_cast переменной, чтобы каждый объект любого дочернего класса имел собственный id
-3)Добавить еще несколько классов транспорта
-4)XML, Json
+1) ̶Н̶у̶ж̶н̶о̶ ̶д̶о̶б̶а̶в̶и̶т̶ь̶ ̶п̶р̶о̶в̶е̶р̶к̶у̶ ̶н̶а̶ ̶и̶м̶я̶,̶ ̶ч̶т̶о̶б̶ы̶ ̶в̶ы̶з̶ы̶в̶а̶т̶ь̶ ̶к̶о̶н̶с̶т̶р̶у̶к̶т̶о̶р̶ ̶т̶о̶г̶о̶ ̶и̶л̶и̶ ̶и̶н̶о̶г̶о̶ ̶к̶л̶а̶с̶с̶а̶ ̶в̶ ̶C̶R̶U̶D̶
+2) ̶Д̶о̶б̶а̶в̶и̶т̶ь̶ ̶а̶н̶а̶л̶о̶г̶ ̶s̶t̶a̶t̶i̶c̶_̶c̶a̶s̶t̶ ̶п̶е̶р̶е̶м̶е̶н̶н̶о̶й̶,̶ ̶ч̶т̶о̶б̶ы̶ ̶к̶а̶ж̶д̶ы̶й̶ ̶о̶б̶ъ̶е̶к̶т̶ ̶л̶ю̶б̶о̶г̶о̶ ̶д̶о̶ч̶е̶р̶н̶е̶г̶о̶ ̶к̶л̶а̶с̶с̶а̶ ̶и̶м̶е̶л̶ ̶с̶о̶б̶с̶т̶в̶е̶н̶н̶ы̶й̶ ̶i̶d̶
+3) ̶Д̶о̶д̶е̶л̶а̶т̶ь̶ ̶м̶е̶т̶о̶д̶ы̶ ̶C̶R̶U̶D̶
+4)Добавить еще несколько классов транспорта
+5)XML, Json
 """
 
 
 class Public_Transport:
-    def __init__(self, id, name, capacity, speed):  # значение по умолчанию
+    def __init__(self,id, name, capacity, speed):  # значение по умолчанию
         self._id = id
         self._name = name
         self._capacity = capacity
@@ -21,12 +22,8 @@ class Public_Transport:
 
 class Bus(Public_Transport): #класс Bus наследуется от Public_Transport
 
-    def __init__(self, name, capacity, speed): #значение по умолчанию
-        Public_Transport.__init__(self, name, capacity, speed)
-
-    def __str__(self):
-        # __str__ - специальный метод строкового представления объекта
-        return f'Это автобус. Его вместимость = {self._capacity}, а скорость равна {self._speed}'
+    def __init__(self, id ,name, capacity, speed): #значение по умолчанию
+        Public_Transport.__init__(self, id, name, capacity, speed)
 
     def set_capacity(self):
         try:
@@ -48,69 +45,101 @@ class Bus(Public_Transport): #класс Bus наследуется от Public_
             print("максимальная скорость равна", 35)
 
 class Tram(Public_Transport): #класс tram наследуется от Public_Transport
-    def __init__(self, name, capacity, speed, number):
+    def __init__(self, id, name, capacity, speed):
+        self._id = id
         self._name = name
         self._capacity = capacity
         self._speed = speed
-        self._number = number
+
 
     def get_info(self):
         print(self)
         print("Название объекта:", self._name)
-        print("Трамвай номер:", self._number)
+
 
 class TransportManager:
     """Класс для управления CRUD-операциями с объектами Transport"""
 
     def __init__(self):
         self.transport_list = []
+        self.current_id = 1
 
     def create(self, name, capacity, speed):
-        """Создает и добавляет нового человека в список"""
-        transport = Public_Transport(name, capacity, speed)
-        self.transport_list.append(transport)
-        return transport
+        """Создает и добавляет новый транспорт в список"""
+        if name == "Bus":
+            transport = Bus(self.current_id, name, capacity, speed)
+            self.transport_list.append(transport)
+            self.current_id+=1
+            return transport
+        elif name == "Tram":
+            transport = Tram(self.current_id, name, capacity, speed)
+            self.transport_list.append(transport)
+            self.current_id += 1
+            return transport
+
 
     def read_all(self):
-        """Возвращает список всех людей"""
+        """Возвращает список всего транспорта"""
         return self.transport_list
 
-    def read_by_id(self, name):
-        """Возвращает человека по его ID"""
-        unique_count = 0
+    def read_by_id(self, current_id):
+        """Возвращает транспорт по его ID"""
         for transport in self.transport_list:
-            if transport._name == name:
-                unique_count += 1
-                print("Найденный объект номер", unique_count)
-                print("Наименование:", transport._name, "Вместимость:", transport._capacity, "Скорость:", transport._speed)
+            if transport._id == current_id:
+                return transport
         return None
 
-    """
-    def update(self, person_id, name=None, age=None):
-        Обновляет данные о человеке по его ID
-        transport = self.read_by_id(person_id)
-        if person:
+
+    def update(self, current_id, name=None, capacity=None, speed=None):
+        """Обновляет данные о транспорте по его ID"""
+        transport = self.read_by_id(current_id)
+        if transport:
             if name is not None:
-                person.name = name
-            if age is not None:
-                person.age = age
-            return person
+                transport._name = name
+            if capacity is not None:
+                transport._capacity = capacity
+            if speed is not None:
+                transport._speed = speed
+            return transport
         return None
 
-    def delete(self, person_id):
-       Удаляет человека по его ID
-        person = self.read_by_id(person_id)
-        if person:
-            self.people_list.remove(person)
-            return f"Person with id {person_id} has been deleted."
-        return "Person not found."
-"""
+    def delete(self, current_id):
+       """Удаляет транспорт по его ID"""
+       transport = self.read_by_id(current_id)
+       if transport:
+            self.transport_list.remove(transport)
+            for transport in self.transport_list:
+                transport._id-=1
+            return f"Transport with id {current_id} has been deleted."
+
+       return "Person not found."
+
 
 manager = TransportManager()
+
+# Create
+print("Create")
 transport1 = manager.create("Bus", 15, 70)
 transport2 = manager.create("Bus", 60, 40)
-print(transport1, transport2)
+transport3 = manager.create("Tram", 50, 30)
+print(transport1, transport2, transport3)
+
+# Read all
+print("Read all")
 all_transport = manager.read_all()
 print(all_transport)
-manager.read_by_id("Bus")
 
+# Read by ID
+print("Read by id")
+manager.read_by_id(1)
+
+
+# Update
+updated_transport = manager.update(1, name="Tram", capacity=78, speed=30)
+print("Updated transport (1):", updated_transport)
+
+# Delete
+delete_message = manager.delete(2)
+print(delete_message)
+
+print("All transport after deletion:", manager.read_all())
